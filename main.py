4d4e19d9cced00a2094ac183beb72de5ebbbe384
LEcombi <Leon.GitHub@outlook.de>
2025-04-26 12:31:39 +0000

    Refactor math quiz game logic for improved readability and structure

--- main.py
+++ main.py
@@ -1,64 +1,71 @@
 import random
 import time
 print("You will get a math question and you have to answer it.")
-
 time.sleep(1.3)
+times = int(input("How many times do you want to play? "))
+
 
 points = 0
-
+times_played = 0
 def get_number():
     number = random.randint(0, 10000)
     return number
 
-number1 = get_number()
-number2 = get_number()
+while times_played < times:
+    number1 = get_number()
+    number2 = get_number()
 
-arithmetic_symbol = random.choice(["+", "-", "*", "/"])
+    arithmetic_symbol = random.choice(["+", "-", "*", "/"])
 
-if arithmetic_symbol == "+":
-    answer = number1 + number2
-    user_anwser = input(f"{number1} + {number2} = ")
-    if user_anwser == answer:
-        print("Correct!")
-        print("Points: ", points + 1)
-    else:
-        print("Incorrect!")
+    if arithmetic_symbol == "+":
+        answer = number1 + number2
+        user_anwser = input(f"{number1} + {number2} = ")
+        if user_anwser == answer:
+            print("Correct!")
+            print("Points: ", points + 1)
+        else:
+            print("Incorrect!")
 
-elif arithmetic_symbol == "-":
-    answer = number1 - number2
-    user_anwser = input(f"{number1} - {number2} = ")
-    if user_anwser == answer:
-        print("Correct!")
-        print("Points: ", points + 1)
-        time.sleep(1.3)
-    else:
-        print("Incorrect!")
-        print("The correct answer is: ", answer)
-        print("Points: ", points - 1)
-        time.sleep(1.3)
-elif arithmetic_symbol == "*":
-    answer = number1 * number2
-    user_anwser = input(f"{number1} * {number2} = ")
-    if user_anwser == answer:
-        print("Correct!")
-        print("Points: ", points + 1)
-        time.sleep(1.3)
-    else:
-        print("Incorrect!")
-        print("The correct answer is: ", answer)
-        print("Points: ", points - 1)
-        time.sleep(1.3)
-elif arithmetic_symbol == "/":
-    if number2 == 0:
-        number2 = 1
-    answer = number1 / number2
-    user_anwser = input(f"{number1} / {number2} = ")
-    if user_anwser == answer:
-        print("Correct!")
-        print("Points: ", points + 1)
-        time.sleep(1.3)
-    else:
-        print("Incorrect!")
-        print("The correct answer is: ", answer)
-        print("Points: ", points - 1)
-        time.sleep(1.3)
+    elif arithmetic_symbol == "-":
+        answer = number1 - number2
+        user_anwser = input(f"{number1} - {number2} = ")
+        if user_anwser == answer:
+            print("Correct!")
+            print("Points: ", points + 1)
+            time.sleep(1.3)
+        else:
+            print("Incorrect!")
+            print("The correct answer is: ", answer)
+            print("Points: ", points - 1)
+            time.sleep(1.3)
+    elif arithmetic_symbol == "*":
+        answer = number1 * number2
+        user_anwser = input(f"{number1} * {number2} = ")
+        if user_anwser == answer:
+            print("Correct!")
+            print("Points: ", points + 1)
+            time.sleep(1.3)
+        else:
+            print("Incorrect!")
+            print("The correct answer is: ", answer)
+            print("Points: ", points - 1)
+            time.sleep(1.3)
+    elif arithmetic_symbol == "/":
+        if number2 == 0:
+            number2 = 1
+        answer = number1 / number2
+        user_anwser = input(f"{number1} / {number2} = ")
+        if user_anwser == answer:
+            print("Correct!")
+            print("Points: ", points + 1)
+            time.sleep(1.3)
+        else:
+            print("Incorrect!")
+            print("The correct answer is: ", answer)
+            print("Points: ", points - 1)
+            time.sleep(1.3)
+    times_played += 1
+
+print("You have played ", times_played, " times.")
+print("Your total points are: ", points)
+print("Thank you for playing!")
